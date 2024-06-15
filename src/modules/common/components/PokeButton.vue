e1a4465871a772f703c0d0c18080914f997a4ccf
Abner Antonio Chamorro <achamorro@pmac-achamorro.local>
2024-06-15 15:56:57 -0500

    modules created

---
+++ src/modules/common/components/PokeButton.vue
@@ -0,0 +1,8 @@
+<template>
+
+    <button
+        class="flex items-center space-x-1.5 rounded-3xl px-4 py-1.5 text-white bg-blue-500 disabled:bg-gray-300 hover:bg-blue-600 transition">
+
+        Get Started
+    </button>
+</template>
---
+++ src/modules/home/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+    <section class="flex flex-col items-center justify-center h-screen">
+
+        <PikachuSvg />
+
+        <h1 class="text-3xl font-bold mb-8">
+            Welcome to Pokédex
+        </h1>
+        <p class="text-center max-w-[520px]">
+            The digital encyclopedia created by Professor Oak is an invaluable tool to Trainers in the Pokémon world.
+        </p>
+        <PokeButton />
+
+    </section>
+</template>
+<script lang="ts" setup>
+import PokeButton from '@/modules/common/components/PokeButton.vue';
+import PikachuSvg from '../components/PikachuSvg.vue';
+</script>
